36874d36d86fe789eb703d6072b6e048428f6ae3
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2021-01-12 12:22:23 -0800

    change lib to class to get types in import context

--- polygon/index.ts
+++ polygon/index.ts
@@ -1,11 +1,11 @@
-let polygon = {
-    verticesEqual: (vertex1: Vertex, vertex2: Vertex): boolean => {
+class Polygon {
+    verticesEqual(vertex1: Vertex, vertex2: Vertex): boolean {
         return vertex1.x == vertex2.x && vertex1.y == vertex2.y;
-    },
-    isPchainClosed: function (chain: Vertex[]): boolean {
+    }
+    isPchainClosed(chain: Vertex[]): boolean {
         return this.verticesEqual(chain[0], chain[chain.length - 1]);
-    },
-    getSlope: function (edge: Edge): number {
+    }
+    getSlope(edge: Edge): number {
         //get index of the vertex with the larger x value
         let xMaxIndex = edge[0].x > edge[1].x ? 0 : 1;
         let v1: Vertex = edge[(xMaxIndex + 1) % 2]
@@ -14,23 +14,23 @@ let polygon = {
         let deltaX: number = v2.x - v1.x;
 
         return deltaY / deltaX;
-    },
-    getLineFromEdge: function (edge: Edge): Line {
+    }
+    getLineFromEdge(edge: Edge): Line {
         let xMaxIndex = edge[0].x > edge[1].x ? 0 : 1;
         let point = edge[xMaxIndex];
         let slope = this.getSlope(edge);
 
         return { point: point, slope: slope };
-    },
-    getIntersection: function (line1: Line, line2: Line): Vertex {
+    }
+    getIntersection(line1: Line, line2: Line): Vertex {
         //lines are in point-slope form
         //solve for x
         let xIntersection: number = ((line2.slope * line2.point.x + line1.point.y) - (line1.slope * line1.point.x + line2.point.y)) / (line2.slope - line1.slope)
         //plug in to formula
         let yIntersection: number = line1.slope * (xIntersection - line1.point.x) + line1.point.y;
         return { x: xIntersection, y: yIntersection };
-    },
-    isPointInRectBounds: function (edge: Edge, point: Vertex): boolean {
+    }
+    isPointInRectBounds(edge: Edge, point: Vertex): boolean {
         let xMin = Math.min(edge[0].x, edge[1].x);
         let xMax = Math.max(edge[0].x, edge[1].x);
         let isInXRange = xMin <= point.x && point.x <= xMax;
@@ -40,32 +40,30 @@ let polygon = {
         let isInYRange = yMin <= point.y && point.y <= yMax;
 
         return isInXRange && isInYRange;
-    },
-    doEdgesIntersect: function (edge1: Edge, edge2: Edge): boolean {
+    }
+    doEdgesIntersect(edge1: Edge, edge2: Edge): boolean {
         let line1: Line = this.getLineFromEdge(edge1);
         let line2: Line = this.getLineFromEdge(edge2);
         //if paralell return false
         if (line1.slope == line2.slope) {
             return false
         }
-
         //get intersection point
         let intersection = this.getIntersection(line1, line2);
-
         //if intersection is in x bounds and in y bounds return true
         let inEdge1Bounds = this.isPointInRectBounds(edge1, intersection)
         let inEdge2Bounds = this.isPointInRectBounds(edge2, intersection);
 
         return inEdge1Bounds && inEdge2Bounds;
-    },
-    getEdges: function (vertices: Vertex[]): Edge[] {
+    }
+    getEdges(vertices: Vertex[]): Edge[] {
         let edges: Edge[] = [];
         for (let i = 0; i < vertices.length - 1; i++) {
             edges.push([vertices[i], vertices[(i + 1) % vertices.length]])
         }
         return edges;
-    },
-    isSelfIntersecting: function (shape: Edge[]): boolean {
+    }
+    isSelfIntersecting(shape: Edge[]): boolean {
         //generate all pairs of edges that should be checked 
         let comparisonsToMake: [Edge, Edge][] = [];
         //only need to loop halfway, comparison order is not important:
@@ -79,11 +77,10 @@ let polygon = {
                 }
             }
         }
-
         //is there a pair of edges that intersects
         let selfIntersects = comparisonsToMake.some((edgePair) => this.doEdgesIntersect(edgePair[0], edgePair[1]));
         return selfIntersects;
     }
 };
 
-module.exports = polygon;
+module.exports = new Polygon();
--- test/unit.ts
+++ test/unit.ts
@@ -1,96 +1,96 @@
 // Dependencies
 const assert = require('assert');
-const lib = require('../polygon')
+const polygon: Polygon = require('../polygon')
 
 // Holder for Tests
 let unit: any = {};
 
 //-----verticesEqual
 unit['verticesEqual'] = (done: () => void) => {
-    assert.strictEqual(lib.verticesEqual({ x: 3, y: 3 }, { x: 3, y: 3 }), true)
+    assert.strictEqual(polygon.verticesEqual({ x: 3, y: 3 }, { x: 3, y: 3 }), true)
     done()
 }
 unit['verticesEqual - unequal'] = (done: () => void) => {
-    assert.strictEqual(lib.verticesEqual({ x: 3, y: 3 }, { x: 4, y: 2 }), false)
+    assert.strictEqual(polygon.verticesEqual({ x: 3, y: 3 }, { x: 4, y: 2 }), false)
     done()
 }
 //-----isPchainClosed-----
 unit['isPchainClosed - closed'] = (done: () => void) => {
-    assert.ok(lib.isPchainClosed([{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 }, { x: 0, y: 0 }]));
+    assert.ok(polygon.isPchainClosed([{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 }, { x: 0, y: 0 }]));
     done()
 }
 unit['isPchainClosed - not closed'] = (done: () => void) => {
-    assert.strictEqual(lib.isPchainClosed([{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 }, { x: -1, y: 0 }]), false);
+    assert.strictEqual(polygon.isPchainClosed([{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 }, { x: -1, y: 0 }]), false);
     done()
 }
 //-----getSlope-----
 unit['getSlope'] = (done: () => void) => {
-    assert.strictEqual(lib.getSlope([{ x: 0, y: 0 }, { x: 1, y: 0 }]), 0);
+    assert.strictEqual(polygon.getSlope([{ x: 0, y: 0 }, { x: 1, y: 0 }]), 0);
     done()
 }
 unit['getSlope - infinity'] = (done: () => void) => {
-    assert.strictEqual(lib.getSlope([{ x: 0, y: 0 }, { x: 0, y: 5 }]), Infinity);
+    assert.strictEqual(polygon.getSlope([{ x: 0, y: 0 }, { x: 0, y: 5 }]), Infinity);
     done()
 }
 unit['getSlope - another test'] = (done: () => void) => {
-    assert.strictEqual(lib.getSlope([{ x: 0, y: 0 }, { x: 4, y: 3 }]), 3 / 4);
+    assert.strictEqual(polygon.getSlope([{ x: 0, y: 0 }, { x: 4, y: 3 }]), 3 / 4);
     done()
 }
 //-----getLineFromEdge-----
 unit['getLineFromEdge'] = (done: () => void) => {
-    assert.deepStrictEqual(lib.getLineFromEdge([{ x: 0, y: 0 }, { x: 4, y: 3 }]), { point: { x: 4, y: 3 }, slope: 3 / 4 });
+    assert.deepStrictEqual(polygon.getLineFromEdge([{ x: 0, y: 0 }, { x: 4, y: 3 }]), { point: { x: 4, y: 3 }, slope: 3 / 4 });
     done()
 }
 //-----getIntersection-----
 unit['getIntersection'] = (done: () => void) => {
-    let line1 = lib.getLineFromEdge([{ x: 0, y: 0 }, { x: 2, y: 2 }]);
-    let line2 = lib.getLineFromEdge([{ x: 0, y: 2 }, { x: 2, y: 0 }]);
-    assert.deepStrictEqual(lib.getIntersection(line1, line2), { x: 1, y: 1 });
+    let line1 = polygon.getLineFromEdge([{ x: 0, y: 0 }, { x: 2, y: 2 }]);
+    let line2 = polygon.getLineFromEdge([{ x: 0, y: 2 }, { x: 2, y: 0 }]);
+    assert.deepStrictEqual(polygon.getIntersection(line1, line2), { x: 1, y: 1 });
     done()
 }
 unit['getIntersection - another one'] = (done: () => void) => {
-    let line1 = lib.getLineFromEdge([{ x: 186, y: 131 }, { x: 342, y: 123 }]);
-    let line2 = lib.getLineFromEdge([{ x: 159, y: 72 }, { x: 303, y: 248 }]);
-    assert.deepStrictEqual(lib.getIntersection(line1, line2), { x: 206.41610738255034, y: 129.95302013422818 });
+    let line1 = polygon.getLineFromEdge([{ x: 186, y: 131 }, { x: 342, y: 123 }]);
+    let line2 = polygon.getLineFromEdge([{ x: 159, y: 72 }, { x: 303, y: 248 }]);
+    assert.deepStrictEqual(polygon.getIntersection(line1, line2), { x: 206.41610738255034, y: 129.95302013422818 });
     done()
 }
 //-----isPointInRectBounds-----
 unit['isPointInRectBounds'] = function (done: () => void) {
     let point = { x: 186, y: 131 };
     let edge: Edge = [{ x: 159, y: 72 }, { x: 303, y: 248 }]
-    assert.ok(lib.isPointInRectBounds(edge, point));
+    assert.ok(polygon.isPointInRectBounds(edge, point));
     done();
 };
 unit['isPointInRectBounds - no'] = function (done: () => void) {
     let point = { x: 304, y: 131 };
     let edge: Edge = [{ x: 159, y: 72 }, { x: 303, y: 248 }]
 
-    assert.strictEqual(lib.isPointInRectBounds(edge, point), false);
+    assert.strictEqual(polygon.isPointInRectBounds(edge, point), false);
     done();
 };
 //-----doEdgesIntersect-----
 unit['doEdgesIntersect - yes'] = function (done: () => void) {
     let edge1: Edge = [{ x: 0, y: 0 }, { x: 1, y: 1 }]
     let edge2: Edge = [{ x: 0, y: 1 }, { x: 1, y: 0 }]
-    assert.ok(lib.doEdgesIntersect(edge1, edge2))
+    assert.ok(polygon.doEdgesIntersect(edge1, edge2))
     done()
 }
 unit['doEdgesIntersect - paralell'] = function (done: () => void) {
     let edge1: Edge = [{ x: 0, y: 0 }, { x: 5, y: 0 }]
     let edge2: Edge = [{ x: 0, y: 1 }, { x: 5, y: 1 }]
-    assert.strictEqual(lib.doEdgesIntersect(edge1, edge2), false)
+    assert.strictEqual(polygon.doEdgesIntersect(edge1, edge2), false)
     done()
 }
 unit['doEdgesIntersect - lines intersect but not in bounds'] = function (done: () => void) {
     let edge1: Edge = [{ x: 0, y: 0 }, { x: 1, y: .5 }]
     let edge2: Edge = [{ x: 0, y: 2 }, { x: 1, y: 1.5 }]
-    assert.strictEqual(lib.doEdgesIntersect(edge1, edge2), false)
+    assert.strictEqual(polygon.doEdgesIntersect(edge1, edge2), false)
     done()
 }
 //----getEdges-----
 unit['getEdges'] = function (done: () => void) {
     let vertices: Vertex[] = [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
-    let edges: Edge[] = lib.getEdges(vertices);
+    let edges: Edge[] = polygon.getEdges(vertices);
     let expectedEdges: Edge[] = [
         [{ x: 0, y: 0 }, { x: 0, y: 1 }],
         [{ x: 0, y: 1 }, { x: 1, y: 1 }],
@@ -103,14 +103,14 @@ unit['getEdges'] = function (done: () => void) {
 //-----isSelfIntersecting-----
 unit['isSelfIntersecting - yes'] = function (done: () => void) {
     let vertices: Vertex[] = [{ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 0.5, y: 2 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
-    let edges: Edge[] = lib.getEdges(vertices);
-    assert.strictEqual(lib.isSelfIntersecting(edges), true)
+    let edges: Edge[] = polygon.getEdges(vertices);
+    assert.strictEqual(polygon.isSelfIntersecting(edges), true)
     done();
 }
 unit['isSelfIntersecting - no'] = function (done: () => void) {
     let vertices: Vertex[] = [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0.5, y: 2 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
-    let edges: Edge[] = lib.getEdges(vertices);
-    assert.strictEqual(lib.isSelfIntersecting(edges), false)
+    let edges: Edge[] = polygon.getEdges(vertices);
+    assert.strictEqual(polygon.isSelfIntersecting(edges), false)
     done();
 }
 
